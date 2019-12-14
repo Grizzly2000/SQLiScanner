703943745bdf311ebf2d3299e83e9c6b6de1e7ac
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-14 19:13:01 +0100

    add target getter

--- modules/SqliScanner.py
+++ modules/SqliScanner.py
@@ -2,7 +2,10 @@ import settings
 
 
 # Main class
-class SqliScanner:
+class SqliScanner(object):
     def __init__(self, target, server=settings.SQLMAP_SERVER):
         self.__target = target
         self.__server = server
+
+    def get_target(self):
+        return self.__target
