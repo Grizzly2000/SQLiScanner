0d4535769f7eded1943366e6fa4dc334b70613c0
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-14 21:21:43 +0100

    define BruteForce class if creds are known, users/passwords files will be ignored

---
+++ modules/BruteForce.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+import settings
+
+
+# Brute force authentication
+class BruteForce(object):
+    def __init__(self, debug: bool, url_login: str, target_credential: str = None,
+                 file_users: str = settings.BF_FILEPATH_USERS, file_passwords: str = settings.BF_FILEPATH_PASSWORDS):
+        self.__debug = debug
+        self.__url_login = url_login
+        self.__target_credential = target_credential
+        self.__cookies = None
+        self.__request_login = None
+
+        # if known creds has not been set, set users/passwords files
+        if not self.__target_credential:
+            self.__file_users = file_users
+            self.__file_passwords = file_passwords
+
