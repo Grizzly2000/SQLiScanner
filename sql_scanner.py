64f8d1aa677a371c0624f39b0171e049c37a6000
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-14 18:58:39 +0100

    add comment + add settings in main function

--- sql_scanner.py
+++ sql_scanner.py
@@ -1,13 +1,24 @@
 import argparse
 import sys
 
+import settings
+
+
+# This program check if a website is vulnerable to SQL injection (wiki: https://en.wikipedia.org/wiki/SQL_injection)
+# It will crawl the targeted website and check if form parameters are vulnerable using SQLmap server API.
+# Then, the result will be written in JSON file
+# The project sqlmap is available here : https://github.com/sqlmapproject/sqlmap.
 
 # This function will parse argument to run the main class.
+# Not required parameters will be loaded in 'settings.py' as default parameters.
+# Here is the list of each parameters :
+# --url is a required parameter. The program will crawl this URL to check if form parameters are vulnerable to SQLi.
+# --sqlmap-server is optional. Provide its own sqlmapapi server. (run sqlmap server : sqlmapapi.py -s)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", help="target to scan. (ex: http://localhost:8000/)", required=True)
     parser.add_argument("-s", "--sqlmap-server",
-                        help="Sqlmap server API. (default: 127.0.0.1:8775)")
+                        help="Sqlmap server API. (default: {default})".format(default=settings.SQLMAP_SERVER))
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
