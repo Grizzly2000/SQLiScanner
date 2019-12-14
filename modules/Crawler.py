1aef6aaab69e0ae593df25e33c425253b94b5003
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-14 23:40:15 +0100

    add comment

--- modules/Crawler.py
+++ modules/Crawler.py
@@ -3,27 +3,21 @@ import logging
 
 
 # Class which crawl URLs website recursively
-# Init paramaters :
-# debug : enable debug mode
-# target : target to scan recursively
-# cookies : specify cookies cookies
-# threshold : crawl to a specific deep
-# urls : list of URL object
-# self.__logger : class logger
 class Crawler(object):
     def __init__(self, debug: bool, target: str, cookies: dict = None, threshold: int = settings.CRAWLER_THRESHOLD):
-        self.__debug = debug
-        self.__target = target
-        self.__cookies = cookies
-        self.__threshold = threshold
-        self.__urls = []
+        self.__debug = debug                        # debug mode
+        self.__target = target                      # target to crawl recursively
+        self.__cookies = cookies                    # cookies to provide
+        self.__threshold = threshold                # crawl to a specific deep
+
+        self.__urls = []                            # store URL objects here
 
         # Init logger
-        self.__logger = logging.getLogger(__name__)
+        self.__logger = logging.getLogger(__name__) # logger
         self.__logger.disabled = not debug
 
     # Find urls with parameters in form
-    def crawl_target_forms(self):
-        self.__logger.debug("Start crawler for {target}".format(target=self.__target))
+    def run(self):
+        self.__logger.info("Start crawler for {target}".format(target=self.__target))
 
-        self.__logger.debug("Exit crawler for {target}".format(target=self.__target))
+        self.__logger.info("Exit crawler for {target}".format(target=self.__target))
