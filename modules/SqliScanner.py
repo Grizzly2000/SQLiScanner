8cbf8a7fb68adaf45a94156c7d2420e273c1b123
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-14 20:01:46 +0100

    add debug mode add run method

--- modules/SqliScanner.py
+++ modules/SqliScanner.py
@@ -1,11 +1,20 @@
 import settings
+import logging
 
 
 # Main class
 class SqliScanner(object):
-    def __init__(self, target, server=settings.SQLMAP_SERVER):
+    def __init__(self, target, debug, server=settings.SQLMAP_SERVER):
         self.__target = target
         self.__server = server
 
+        # Init logger
+        self.__logger = logging.getLogger(__name__)
+        self.__logger.disabled = not debug
+
     def get_target(self):
         return self.__target
+
+    def run(self):
+        self.__logger.debug("Start scan for {target}".format(target=self.__target))
+        self.__logger.debug("End scan for {target}".format(target=self.__target))
