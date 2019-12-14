92740fe7b510aacc589155c8e2e62660a9fb66ea
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-14 23:40:32 +0100

    add dvwa settings + debug level

--- settings.py
+++ settings.py
@@ -1,5 +1,26 @@
+# Debug
+DEBUG_LEVEL = 20  # logging.DEBUG = 10 / logging.INFO = 20
+
+# Sqlmap
 SQLMAP_SERVER = "127.0.0.1:8775"
+
+# HTTP
 CRAWLER_THRESHOLD = 10
-BF_FILEPATH_USERS = "users.txt"
-BF_FILEPATH_PASSWORDS = "passwords.txt"
 REQUEST_TIMEOUT = 10
+
+# Brute force files
+BF_FILEPATH_USERS = "wordlists/users.txt"
+BF_FILEPATH_PASSWORDS = "wordlists/passwords.txt"
+
+# DVWA login settings
+URL_LOGIN = "login.php"
+URL_SUCCESS = "index.php"
+COOKIES_EXTRA = {
+    "security": "low"
+}
+COOKIE_SESSION_NAME = "PHPSESSID"
+COOKIE_SESSION_REGEX = COOKIE_SESSION_NAME + "=(.*?);"
+PARAM_FORM_USER_KEY = "username"
+PARAM_FORM_PASSWORD_KEY = "password"
+PARAM_FORM_CSRF_KEY = "user_token"
+PARAM_FORM_LOGIN_KEY = "Login"
