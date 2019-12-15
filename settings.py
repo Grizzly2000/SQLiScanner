abe73a0a3244d8295b6c7c66f590d21e8528394d
Raphaël Pion <raphael.pion@cyberactionlab.net>
2019-12-15 05:33:48 +0100

    add settings: SQLMAP_SERVER is an URL SQLTASK_MAX_TIME_ELAPSED, leave scan if time is up SQLTASK_GET_STATUS_INTERVAL, get status of a scan every X seconds SQLMAP_REPORT_OUTPUT_FILE, write to this file the vulnerabities DEFAULT_INPUT_VALUE, default value to submit for each GET/POST parameter URLS_WHITELIST, avoid to scan these URL

--- settings.py
+++ settings.py
@@ -2,10 +2,13 @@
 DEBUG_LEVEL = 20  # logging.DEBUG = 10 / logging.INFO = 20
 
 # Sqlmap
-SQLMAP_SERVER = "127.0.0.1:8775"
+SQLMAP_SERVER = "http://127.0.0.1:8775/"
+SQLTASK_MAX_TIME_ELAPSED = 30
+SQLTASK_GET_STATUS_INTERVAL = 5
+SQLMAP_REPORT_OUTPUT_FILE = "vulns_report.txt"
 
 # HTTP
-CRAWLER_THRESHOLD = 10
+CRAWLER_THRESHOLD = 30
 REQUEST_TIMEOUT = 10
 
 # Brute force files
@@ -24,3 +27,5 @@ PARAM_FORM_USER_KEY = "username"
 PARAM_FORM_PASSWORD_KEY = "password"
 PARAM_FORM_CSRF_KEY = "user_token"
 PARAM_FORM_LOGIN_KEY = "Login"
+DEFAULT_INPUT_VALUE = "1"  # default value to set in form parameters
+URLS_WHITELIST = ["logout.php", "setup.php", "login.php", "security.php"]
